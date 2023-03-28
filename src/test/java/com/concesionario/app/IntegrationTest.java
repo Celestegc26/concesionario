d871c600152ca99ff2dfd2b19812b40f2f99ae89
Celestegc26 <yuligonzalez26@gmail.com>
2023-03-28 09:33:14 +0200

    Initial version of concesionario generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/concesionario/app/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.concesionario.app;
+
+import com.concesionario.app.ConcesionarioApp;
+import com.concesionario.app.config.AsyncSyncConfiguration;
+import com.concesionario.app.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ConcesionarioApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
